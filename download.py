5a44409defe89806866e314fe93e203b6669db44
Andres Vazquez <andres@data99.com.ar>
2019-12-07 18:36:00 -0300

    download data

--- download.py
+++ download.py
@@ -6,6 +6,7 @@ import json
 import os
 import requests
 import sys
+from slugify import slugify
 
 
 logger = logging.getLogger(__name__)
@@ -22,6 +23,10 @@ save_pages_to = 'json_pages'
 next_data = base_url + data_url
 
 full_datasets = []
+# los tipos de datos se infieren por el campo "icon" de los recursos
+icons = set()  # tipos de datos. Para saber si son descargables
+# no es exacto pero estos serían los descargables (otros son referencias a web externas, repositorios de github, etc)
+allowed_download_icons = ['json', 'ods', 'image', 'jpeg', 'kmz', 'csv', 'pdf', 'shp', 'docx', 'doc', 'kml', 'xls', 'xlsx', 'jpg', 'txt', 'zip']
 datasets = 0
 versiones = 0
 recursos = 0
@@ -55,15 +60,33 @@ while next_data:
             for recurso in version['recursos']:
                 recursos += 1
                 logger.info('   - Recurso: {}'.format(recurso['titulo']))
-                logger.info('     + Icon: {}'.format(recurso['icon']))
+                icon = recurso['icon']
+                logger.info('     + Icon: {}'.format(icon))
+                icons.add(icon)
                 if recurso['url'] is not None:
                     url = recurso['url'] if recurso['url'].startswith('http') else base_url + recurso['url']
                 else:
                     url = None
                 logger.info('     + URL: {}'.format(url))
-                
+
+                # ver si es algo decargable
+                if url and icon in allowed_download_icons:
+                    # que lastima que no hay IDs ...
+                    no_unique_name = '{}---{}---{}.{}'.format(dataset['titulo'], version['titulo'], recurso['titulo'], icon)
+                    dest = 'data/{}'.format(slugify(no_unique_name))
+                    if os.path.isfile(dest):
+                        logger.info(f'Already downloaded {no_unique_name} from {url}')
+                        continue
+                    logger.info(f'#### DOWNLOADING {no_unique_name} from {url}')
+                    data_response = requests.get(url, allow_redirects=True)
+                    f = open(dest, 'wb')
+                    f.write(data_response.content)
+                    f.close()
 
     next_data = data['next']
-
+    logger.info('###################')
+    logger.info(f'Procesados: {datasets} datasets, {versiones} versiones y {recursos} recursos')
+    logger.info('###################')
 
 logger.info(f'Finalizado: {datasets} datasets, {versiones} versiones y {recursos} recursos')
+logger.info(f'Icons {icons}')
